31173667edb10dc146dec2574f97bdf41429de64
silver-belt <silver-belt@nowhere-special.com>
2025-08-02 23:51:46 +0200

    adapted kill script to the added nginx

--- kill-comfyui.sh
+++ kill-comfyui.sh
@@ -2,5 +2,8 @@
 # kill-comfyui.sh
 set -e
 echo ">>> Killing all ComfyUI containers"
-podman ps --filter name=comfyui_ -q | xargs -r podman rm -f
+podman ps --filter name=comfyui_ -q | while read cid; do
+    echo "Removing container $cid"
+    podman rm -f --depend $cid || true
+done
 
